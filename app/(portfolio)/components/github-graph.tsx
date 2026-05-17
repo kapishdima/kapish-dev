e868395d647ee54031449c207c931f25ba8febf3
dev_wandry <dima.development@wandry.com.ua>
2026-05-17 14:20:43 +0300

    feat: refactor github contributions graph

--- app/(portfolio)/components/github-graph.tsx
+++ app/(portfolio)/components/github-graph.tsx
@@ -1,12 +1,6 @@
 import { GitHubContributions } from "@/components/github-contributions";
 import { getCachedContributions } from "@/lib/data/get-cached-contributions";
 
-const contributions = getCachedContributions("kapishdima");
-const profileUrl = "https://github.com/kapishdima";
-
 export const GithubGraph = () => (
-  <GitHubContributions
-    contributions={contributions}
-    githubProfileUrl={profileUrl}
-  />
+  <GitHubContributions contributions={getCachedContributions("kapishdima")} />
 );
