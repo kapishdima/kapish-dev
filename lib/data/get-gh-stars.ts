9f78541f4add921d03e35ee2f42539b1531ee726
dev_wandry <dima.development@wandry.com.ua>
2026-06-09 12:55:45 +0300

    feat: added gh-contribution scroll

--- lib/data/get-gh-stars.ts
+++ lib/data/get-gh-stars.ts
@@ -9,7 +9,6 @@ export const getStargazerCount = async (repo: string): Promise<number> => {
       headers: {
         Accept: "application/vnd.github+json",
         Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`,
-        "X-GitHub-Api-Version": "2022-11-28",
       },
     });
 
